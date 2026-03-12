2cf24208f87ca76de1574a0e8848e700448c2f85
Ashitosh0302 <ashitoshshirsath9a62@gmail.com>
2026-03-12 17:51:47 +0530

    UPDATE THE ENV FILE TO DEPLOY

--- config/db.js
+++ config/db.js
@@ -1,18 +1,21 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection(
-{
+const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect((err) =>
 {
     if(err)
     {
-        console.log("Database connection failed");
+        console.log("Database connection failed:", err);
     }
     else
     {
